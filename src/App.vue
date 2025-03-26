4b17be1fc318e3408fb041062ba99d8b24bb9799
Siow Ming Kang <62229606+MingKangS@users.noreply.github.com>
2025-03-27 01:07:57 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,190 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { ref, onMounted, watch, computed, onUnmounted } from "vue";
+import axios from "axios";
+import FiltersModal from "./components/FiltersModal.vue";
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+const showModal = ref(false);
+const appliedFilters = ref({});
+const categoryIdToNameMap = ref({});
+const orders = ref([]);
+const sortedOrders = computed(() => {
+  return [...orders.value].sort((a, b) => {
+    return typeof a[sortBy.value] == "number"
+      ? a[sortBy.value] - b[sortBy.value]
+      : a[sortBy.value].localeCompare(b[sortBy.value]);
+  });
+});
+const sortBy = ref("object_id");
+const newOrder = ref({
+  customer_name: "",
+  status: "Processing",
+  category_id: "",
+  country: "",
+});
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const fetchOrdersIntervalId = ref("");
+
+const setFilters = (filters) => {
+  let newFilters = { ...filters };
+
+  if (newFilters.category?.includes("Others")) {
+    newFilters.category = [
+      ...newFilters.category,
+      ...Object.values(categoryIdToNameMap.value).filter(
+        (category) => !["Electronics", "Furniture"].includes(category)
+      ),
+    ];
   }
-}
+  appliedFilters.value = newFilters;
+};
+
+const openFilterModal = () => {
+  showModal.value = true;
+};
+
+const closeFilterModal = () => {
+  showModal.value = false;
+};
+
+const fetchCategories = async () => {
+  try {
+    const response = await axios.get("http://localhost:3000/categories");
+    const newCategoryIdToNameMap = {};
+    response.data.forEach((category) => {
+      newCategoryIdToNameMap[category.object_id] = category.name;
+    });
+    categoryIdToNameMap.value = newCategoryIdToNameMap;
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+  }
+};
+
+const fetchOrders = async () => {
+  try {
+    let query = new URLSearchParams(appliedFilters.value).toString();
+    const response = await axios.get(`http://localhost:3000/orders?${query}`);
+    orders.value = response.data;
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+  }
+};
+
+const setSortBy = (newSortBy) => {
+  sortBy.value = newSortBy;
+};
+
+const createOrder = async () => {
+  try {
+    await axios.post("http://localhost:3000/orders", newOrder.value);
+    alert("Order created successfully!");
+    fetchOrders();
+    newOrder.value = {
+      customer_name: "",
+      status: "Processing",
+      category_id: "",
+      country: "",
+    };
+  } catch (error) {
+    console.error("Error creating order:", error);
+  }
+};
+
+watch(appliedFilters, fetchOrders, { deep: true });
+
+onMounted(() => {
+  fetchCategories();
+  fetchOrders();
+  fetchOrdersIntervalId.value = setInterval(fetchOrders, 60 * 1000);
+});
+
+onUnmounted(() => {
+  clearInterval(fetchOrdersIntervalId.value);
+});
 </script>
 
+<template>
+  <div class="container">
+    <button class="open-filter-button" @click="openFilterModal">
+      Open Filters
+    </button>
+    <h2>Orders</h2>
+    <table>
+      <thead>
+        <tr>
+          <th @click="setSortBy('object_id')">ID</th>
+          <th @click="setSortBy('customer_name')">Customer</th>
+          <th @click="setSortBy('status')">Status</th>
+          <th @click="setSortBy('category_id')">Category</th>
+          <th @click="setSortBy('country')">Country</th>
+          <th @click="setSortBy('created_date')">Created Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="order in sortedOrders" :key="order.object_id">
+          <td>{{ order.object_id }}</td>
+          <td>{{ order.customer_name }}</td>
+          <td>{{ order.status }}</td>
+          <td>{{ categoryIdToNameMap[order.category_id] }}</td>
+          <td>{{ order.country }}</td>
+          <td>{{ new Date(order.created_date).toLocaleDateString() }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h2>Create New Order</h2>
+    <form @submit.prevent="createOrder">
+      <input
+        v-model="newOrder.customer_name"
+        placeholder="Customer Name"
+        required
+      />
+      <select v-model="newOrder.category_id" required>
+        <option value="" disabled>Select Category</option>
+        <option
+          v-for="category in categories"
+          :key="category.object_id"
+          :value="category.object_id"
+        >
+          {{ category.name }}
+        </option>
+      </select>
+      <input v-model="newOrder.country" placeholder="Country" required />
+      <button type="submit">Create Order</button>
+    </form>
+    <FiltersModal
+      v-if="showModal"
+      :setFilters="setFilters"
+      @close="closeFilterModal"
+    />
+  </div>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  max-width: 800px;
+  margin: 20px auto;
+  font-family: Arial, sans-serif;
+  display: flex;
+  flex-direction: column;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-bottom: 20px;
+}
+
+.open-filter-button {
+  margin: 20px 20px 20px auto;
+}
+
+th,
+td {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+th {
+  background: #f4f4f4;
 }
 </style>
---
+++ src/components/FiltersModal.vue
@@ -0,0 +1,208 @@
+<script setup>
+import { ref, defineEmits, defineProps, onMounted, watch } from "vue";
+import axios from "axios";
+
+const props = defineProps({
+  setFilters: Function,
+});
+
+defineEmits(["close"]);
+
+const filterValues = ref({
+  customerName: [],
+  country: [],
+});
+
+const filters = ref({
+  createdDateStart: "",
+  createdDateEnd: "",
+  customerName: "",
+  status: ["Open", "Processing", "Accepted", "Rejected"],
+  category: ["Electronics", "Furniture", "Others"],
+  country: "",
+});
+
+const isAllCategorySelected = ref(true);
+const isAllStatusSelected = ref(true);
+
+const isConfirmCloseModal = ref(false);
+
+const fetchFilterValues = async () => {
+  try {
+    const response = await axios.get("http://localhost:3000/orders/filters");
+    filterValues.value = response.data;
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+  }
+};
+
+const applyFilters = () => {
+  props.setFilters({ ...filters.value });
+};
+
+const onToggleAllStatusSelected = (e) => {
+  isAllStatusSelected.value = e.target.checked;
+  if (isAllStatusSelected.value) {
+    filters.value.status = ["Open", "Processing", "Accepted", "Rejected"];
+  } else {
+    filters.value.status = [];
+  }
+};
+
+const onToggleAllCategorySelected = (e) => {
+  isAllCategorySelected.value = e.target.checked;
+  if (isAllCategorySelected.value) {
+    filters.value.category = ["Electronics", "Furniture", "Others"];
+  } else {
+    filters.value.category = [];
+  }
+};
+
+const resetFilters = () => {
+  filters.value = {
+    createdDateStart: "",
+    createdDateEnd: "",
+    customerName: "",
+    status: ["Open", "Processing", "Accepted", "Rejected"],
+    category: ["Electronics", "Furniture", "Others"],
+    country: "",
+  };
+};
+
+const onCloseClicked = () => {
+  isConfirmCloseModal.value = true;
+};
+
+const cancelClose = () => {
+  isConfirmCloseModal.value = false;
+};
+
+watch(
+  filters,
+  (newFilters) => {
+    localStorage.setItem("filters", JSON.stringify(newFilters));
+  },
+  { deep: true }
+);
+
+onMounted(() => {
+  fetchFilterValues();
+  const storedFilters = localStorage.getItem("filters");
+  if (storedFilters) {
+    filters.value = JSON.parse(storedFilters);
+  }
+});
+</script>
+
+<template>
+  <div v-if="isConfirmCloseModal" class="modal">
+    <div class="modal-content">
+      <p>Are you sure you want to navigate away?</p>
+      <button @click="$emit('close')">Yes</button>
+      <button @click="cancelClose">No</button>
+    </div>
+  </div>
+  <div v-else class="modal">
+    <div class="modal-content">
+      <h2>Filters</h2>
+      <h3>Select criteria filter in listing</h3>
+
+      <div class="filter-group">
+        <label>Created Date: </label>Display range from
+        <input v-model="filters.createdDateStart" type="text" /> to
+        <input v-model="filters.createdDateEnd" type="text" />
+      </div>
+
+      <div class="filter-group">
+        <label>Customer Name:</label>
+        <select v-model="filters.customerName">
+          <option
+            v-for="name in filterValues.customerName"
+            :value="name"
+            :key="name"
+          >
+            {{ name }}
+          </option>
+        </select>
+      </div>
+
+      <div class="filter-group">
+        <label>Status:</label>
+        <input type="checkbox" @input="onToggleAllStatusSelected" value="All" />
+        All <input type="checkbox" v-model="filters.status" value="Open" /> Open
+        <input type="checkbox" v-model="filters.status" value="Processing" />
+        Processing
+        <input type="checkbox" v-model="filters.status" value="Accepted" />
+        Accepted
+        <input type="checkbox" v-model="filters.status" value="Rejected" />
+        Rejected
+      </div>
+
+      <div class="filter-group">
+        <label>Category:</label>
+        <input
+          type="checkbox"
+          @input="onToggleAllCategorySelected"
+          value="All"
+        />
+        All
+        <input type="checkbox" v-model="filters.category" value="Electronics" />
+        Electronics
+        <input type="checkbox" v-model="filters.category" value="Furniture" />
+        Furniture
+        <input type="checkbox" v-model="filters.category" value="Others" />
+        Others
+      </div>
+
+      <div class="filter-group">
+        <label>Country:</label>
+        <select v-model="filters.country">
+          <option
+            v-for="country in filterValues.country"
+            :value="country"
+            :key="country"
+          >
+            {{ country }}
+          </option>
+        </select>
+      </div>
+
+      <div class="modal-actions">
+        <button @click="resetFilters">Reset</button>
+        <button @click="applyFilters">Apply</button>
+        <button @click="onCloseClicked">Close</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  width: 400px;
+  border-radius: 8px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.filter-group {
+  margin-bottom: 15px;
+}
+
+.modal-actions {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
